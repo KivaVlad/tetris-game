88d4979822cca841ad531777640841ad9aa6b130
Vladislav <melnikovv717@gmail.com>
2023-12-07 14:25:01 +0300

    create tetris

---
+++ js/gameMenu.js
@@ -0,0 +1,16 @@
+import { menuContent } from "./gameContents.js";
+
+export const createGameMenu = (app) => {
+    const gameContent = document.querySelector('.game-content')
+    gameContent.innerHTML = ''
+    gameContent.innerHTML = menuContent
+
+    const easyBtn = document.querySelector('.easy')
+    const normBtn = document.querySelector('.norm')
+    const hardBtn = document.querySelector('.hard')
+
+    easyBtn.addEventListener('click', () => app(35))
+    normBtn.addEventListener('click', () => app(15))
+    hardBtn.addEventListener('click', () => app(5))
+
+}
